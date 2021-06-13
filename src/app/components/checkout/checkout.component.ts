1411e36fc8c4dd4a48b6a3536d4e3b35f591ecb7
fatih eren <fatihhernn@gmail.com>
2021-06-13 15:26:45 +0300

    Populate Credit Card Dates

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {AngularShopFormService} from '../../services/angular-shop-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -13,7 +14,10 @@ export class CheckoutComponent implements OnInit {
   totalPrice:number=0;
   totalQuantity:number=0;
 
-  constructor(private formBuilder:FormBuilder) { }
+  creditCardYears:number[]=[]
+  creditCardMonths:number[]=[]
+
+  constructor(private formBuilder:FormBuilder,private angularShopFormService:AngularShopFormService) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup=this.formBuilder.group({
@@ -47,6 +51,23 @@ export class CheckoutComponent implements OnInit {
         expirationYear:[""],
       })
     })
+
+    //populate credit card months
+    const startMonth:number=new Date().getMonth()+1;
+    console.log("start month : "+startMonth)
+
+    this.angularShopFormService.getCreditCardMonths(startMonth).subscribe(data=>{
+      console.log("Retrieved credit card months : "+ JSON.stringify(data))
+      this.creditCardMonths=data
+    })
+
+    //populate credit card years
+    this.angularShopFormService.getCreditCardYears().subscribe(data=>{
+      console.log("Retrieved credit card years : "+ JSON.stringify(data))
+      this.creditCardYears=data
+    })
+
+
   }
 
   onSubmit(){
@@ -61,5 +82,27 @@ export class CheckoutComponent implements OnInit {
     }else{
       this.checkoutFormGroup.controls.billingAddress.reset();
     }
+  }
+
+  handleMonthsAndYears() {
+    const creditCardFormGroup=this.checkoutFormGroup.get('creditCard')
+
+    const currentYear:number=new Date().getFullYear()
+    const selectedYear:number=Number(creditCardFormGroup.value.creditCardYears);
+
+    //if the current year equals the selected year, then start with current month
+    let startMonth:number
+
+    if (currentYear==selectedYear){
+      startMonth=new Date().getMonth()+1;
+    }else {
+      startMonth=1
+    }
+
+    this.angularShopFormService.getCreditCardMonths(startMonth).subscribe(data=>{
+      console.log("Retrieved credit card months : "+JSON.stringify(data))
+      this.creditCardMonths=data
+    })
+
   }
 }
